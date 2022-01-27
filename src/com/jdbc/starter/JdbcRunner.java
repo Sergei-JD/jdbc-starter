2d8ff0daeda60221c98b5188f1d65cedf81d3fb4
Sergei-JD <sergshlyazhko@gmail.com>
2022-01-27 23:12:21 +0300

    init commit

---
+++ src/com/jdbc/starter/JdbcRunner.java
@@ -0,0 +1,28 @@
+package com.jdbc.starter;
+
+import com.jdbc.starter.util.ConnectionManager;
+import org.postgresql.Driver;
+
+import java.sql.SQLException;
+
+public class JdbcRunner {
+
+    public static void main(String[] args) throws SQLException {
+        Class<Driver> driverClass = Driver.class;
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info
+                (
+                    id SERIAL PRIMARY KEY ,
+                    data TEXT NOT NULL 
+                );
+                """;
+
+        try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
+            System.out.println(connection.getSchema());
+            System.out.println(connection.getTransactionIsolation());
+            var executeResult = statement.execute(sql);
+            System.out.println(executeResult);
+        }
+    }
+}
