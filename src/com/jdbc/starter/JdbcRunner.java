7d793fa4b55331221d4872c480a322c027a51115
Sergei-JD <sergshlyazhko@gmail.com>
2022-01-28 11:12:58 +0300

    added transaction

--- src/com/jdbc/starter/JdbcRunner.java
+++ src/com/jdbc/starter/JdbcRunner.java
@@ -1,28 +1,96 @@
 package com.jdbc.starter;
 
 import com.jdbc.starter.util.ConnectionManager;
-import org.postgresql.Driver;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
-        Class<Driver> driverClass = Driver.class;
+//        Long flightId = 2L;
+//        var result = getTicketByFlightId(flightId);
+//        System.out.println(result);
+
+//        var result = getFlightBetween(LocalDate.of(2020, 1, 1).atStartOfDay(), LocalDateTime.now());
+//        System.out.println(result);
+
+        checkMetaData();
+    }
+
+    private static void checkMetaData() throws SQLException {
+        try (var connection = ConnectionManager.open()) {
+            var metaData = connection.getMetaData();
+            var catalogs = metaData.getCatalogs();
+
+            while (catalogs.next()) {
+                var catalog = catalogs.getString(1);
+                var schemas = metaData.getSchemas();
+
+                while (schemas.next()) {
+                    var schema = schemas.getString("TABLE_SCHEM");
+                    var tables = metaData.getTables(catalog, schema, "%", new String[] {"TABLE"});
+                    if (schema.equals("public"))
+
+                        while (tables.next()) {
+                        System.out.println(tables.getString("TABLE_NAME"));
+                    }
+                }
+            }
+        }
+    }
+
+    private static List<Long> getFlightBetween(LocalDateTime start, LocalDateTime end) throws SQLException {
         String sql = """
-                CREATE TABLE IF NOT EXISTS info
-                (
-                    id SERIAL PRIMARY KEY ,
-                    data TEXT NOT NULL 
-                );
+                SELECT id
+                FROM flight
+                WHERE departure_date BETWEEN ? AND ?
                 """;
 
-        try (var connection = ConnectionManager.open();
-             var statement = connection.createStatement()) {
-            System.out.println(connection.getSchema());
-            System.out.println(connection.getTransactionIsolation());
-            var executeResult = statement.execute(sql);
-            System.out.println(executeResult);
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnectionManager.open()) {
+            var preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setFetchSize(50);
+            preparedStatement.setQueryTimeout(10);
+            preparedStatement.setMaxRows(100);
+
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(start));
+            System.out.println(preparedStatement);
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(end));
+            System.out.println(preparedStatement);
+
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                result.add(resultSet.getLong("id"));
+            }
         }
+
+        return result;
+    }
+
+    private static List<Long> getTicketByFlightId(Long flightId) throws SQLException {
+        String sql = """
+                SELECT id
+                FROM ticket
+                WHERE flight_id = ?
+                """;
+
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, flightId);
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+//                result.add(resultSet.getLong("id"));
+                result.add(resultSet.getObject("id", Long.class)); //NULL safe
+            }
+        }
+
+        return result;
     }
 }
